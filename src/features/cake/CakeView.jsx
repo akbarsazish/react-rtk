39386eaca8e901645ed57a84545d60758bca95d1
akbarsazish <sazishaliakbar@gmail.com>
2024-03-29 18:12:34 +0330

    add useDispatch to dispatch action from component to readux store

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from'react-redux'
+import { useSelector, useDispatch } from'react-redux'
+import {ordered, restocked} from './cakeSlice'
 
 export const CakeView = () => {
   const numofCakes = useSelector(state => state.cake.numofCakes)
+  const dispatch = useDispatch()
   return (
     <div>
         <h2> Number of cakes: {numofCakes} </h2>
-        <button>Order cake </button>
-        <button>Restock cake </button>
+        <button onClick={() => dispatch(ordered())} >Order cake  </button>
+        <button onClick={() => dispatch(restocked(3))} >Restock cake </button>
     </div>
   )
 }
--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import  {useSelector}  from 'react-redux'
+
+import { useState } from 'react'
+import  {useSelector, useDispatch}  from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
+
 
 export const IcecreamView = () => {
-
+  const [value, setValue] = useState(1)
   const numofIcecreams = useSelector(state => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
   return (
     <div>
         <h2> Number of Icecream: {numofIcecreams} </h2>
-        <button>Order icecream </button>
-        <button>Restock icecream </button>
+        <button onClick={()=> dispatch(ordered())}>Order icecream </button>
+        <input type="number" value={value} onChange={e => setValue(parseInt(e.target.value))}/>
+        <button onClick={()=> dispatch(restocked(value))}>Restock icecream </button>
     </div>
   )
-}
+} 
